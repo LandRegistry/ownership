41f89ba75d33561a59ee85cfeacf34ad3af7df88
Nicolas Devetak <nicolasdevetak@gds3619.local>
2014-09-05 13:16:51 +0100

    explicitly set settings a dev env no longer sets these

--- create_test_owners.sh
+++ create_test_owners.sh
@@ -3,5 +3,5 @@
 source /vagrant/script/dev-env-functions
 source ./environment.sh
 create_virtual_env "ownership"
-python manage.py create_owner --lrid='2fd71646-7ebb-4b90-89d0-1a0221aafbff' --owner_index='1' --title_number='DN300'
+SETTINGS=config.DevelopmentConfig python manage.py create_owner --lrid='2fd71646-7ebb-4b90-89d0-1a0221aafbff' --owner_index='1' --title_number='DN300'
 deactivate
