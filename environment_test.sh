db8824eb641b5842275e3d35e3dd8d1f55372595
Adam Shimali <adam.shimali@gmail.com>
2014-10-06 11:43:30 +0100

    Unpin requirements. Clean up env scripts

--- environment_test.sh
+++ environment_test.sh
@@ -1,5 +1,7 @@
-# Here we'll create a test database, and override the database to the test values.
+export SETTINGS='config.TestConfig'
 export APP_NAME="ownership_test"
+
+# Here we'll create a test database, and override the database to the test values.
 set +o errexit
 createuser -s $APP_NAME
 createdb -U $APP_NAME -O $APP_NAME $APP_NAME -T template0
